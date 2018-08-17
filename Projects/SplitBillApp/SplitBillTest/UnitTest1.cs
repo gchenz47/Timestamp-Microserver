81db88d31a1d2f99df9939b673263932c0e813aa
gchenz47 <cz1989117@gmail.com>
2018-08-17 13:10:31 -0400

    t 	# Please enter the commit message for your changes. Lines starting

---
+++ Projects/SplitBillApp/SplitBillTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SplitBillTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
